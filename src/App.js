100a5b6ecadcfc645c0b8e68024381de2432b817
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-13 14:13:32 -0400

    Add action works properly

--- src/App.js
+++ src/App.js
@@ -1,68 +1,53 @@
 import React, { useState, useReducer } from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
+import { initialState, todoReducer } from "./reducers/todoReducer";
 import "./components/TodoComponents/Todo.css";
 
-function todoReducer(state, action) {
-  return state;
-}
-
-const initialState = {};
 function App() {
-  const [task, setTask] = useState("");
-  const [todo, setTodo] = useState([
-    {
-      task: "Example Todo",
-      id: 1528817077286,
-      completed: false
-    }
-  ]);
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  console.log("state inside App", state.todoData);
 
   const clearTodos = () => {
-    setTodo([
-      {
-        task: "Example Todo",
-        id: 1528817077286,
-        completed: false
-      }
-    ]);
-  };
-
-  const addTodos = event => {
-    event.preventDefault();
-    setTask({});
-    setTodo([...todo, task]);
+    dispatch({ type: "clearTodo" });
   };
 
   const handleChange = event => {
     event.preventDefault();
-    setTask({
-      [event.target.name]: event.target.value,
-      id: Date.now(),
-      completed: false
+    dispatch({
+      type: "field",
+      field: "task",
+      value: event.target.value
     });
+  };
+
+  const addTodos = event => {
+    event.preventDefault();
+    dispatch({ type: "addTodo" });
   };
 
   const deleteItem = id => {
     console.log("deleted");
-    const filteredItems = todo.filter(item => {
+    const filteredItems = state.todoData.filter(item => {
       return item.id !== id;
     });
-    setTodo(filteredItems);
+    state.todoData = filteredItems;
+    console.log(state.todoData);
   };
+
+  console.log("state after delete", state.todoData);
 
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  console.log("todo", todo);
   return (
     <div>
       <h1>Tasks</h1>
-      <TodoList toDoList={todo} deleteItem={deleteItem} />
+      <TodoList toDoList={state.todoData} deleteItem={deleteItem} />
       <TodoForm
         clear={clearTodos}
         add={addTodos}
-        toDoList={todo}
+        toDoList={state.todoData}
         handleChange={handleChange}
       />
     </div>
