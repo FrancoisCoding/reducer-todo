c6010e5406d6a4db08a222b785b280f4073d41ad
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-13 15:33:25 -0400

    Created functions that will run dispatch

--- reducer-practice/src/App.js
+++ reducer-practice/src/App.js
@@ -3,6 +3,17 @@ import "./App.css";
 import { initialState, todoReducer } from "./reducers/todoReducer.js";
 
 function App() {
+  // Initialize useReducer
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  // Setup Functions that run dispatch which will be used to trigger actions and pass in parameters with payload
+  const toggleCompleted = id =>
+    dispatch({ type: "TOGGLE_COMPLETED", payload: id });
+  const addTodo = task => dispatch({ type: "ADD_TODO", payload: task });
+  const clear = () => dispatch({ type: "CLEAR" });
+  const handleChange = event =>
+    dispatch({ type: "UPDATE", payload: event.target.value });
+
   return (
     <div className="App">
       <h1>Start</h1>
